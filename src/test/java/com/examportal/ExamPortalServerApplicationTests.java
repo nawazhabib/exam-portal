945744893c567cbdd365d37d32287e9db8eb92a8
Habib-qazwsx <roaringhabib64@gmail.com>
2022-06-09 18:42:56 +0600

    setup project for start coding

---
+++ src/test/java/com/examportal/ExamPortalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamPortalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
